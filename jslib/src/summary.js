6f66f4cedd04d5fc2c9caa4e3f8642cb6b6c9f99
Scott Came <scottcame10@gmail.com>
2018-02-26 22:08:07 -0800

    Added more box score stats

--- jslib/src/summary.js
+++ jslib/src/summary.js
@@ -20,16 +20,32 @@ function getGameSummaryToPlay(game, playIndex) {
   ret.visitor_team_stats = new Object;
   ret.visitor_team_stats.score = 0;
   ret.visitor_team_stats.hits = 0;
-  ret.visitor_team_stats.errors = 0;
+  ret.visitor_team_stats.error_count = 0;
   ret.visitor_team_stats.runs_per_inning = [];
   ret.visitor_team_stats.lob = 0;
+  ret.visitor_team_stats.dp = [];
+  ret.visitor_team_stats.doubles = [];
+  ret.visitor_team_stats.triples = [];
+  ret.visitor_team_stats.hr = [];
+  ret.visitor_team_stats.hbp = [];
+  ret.visitor_team_stats.sb = [];
+  ret.visitor_team_stats.cs = [];
+  ret.visitor_team_stats.errors = [];
 
   ret.home_team_stats = new Object;
   ret.home_team_stats.score = 0;
   ret.home_team_stats.hits = 0;
-  ret.home_team_stats.errors = 0;
+  ret.home_team_stats.error_count = 0;
   ret.home_team_stats.runs_per_inning = [];
   ret.home_team_stats.lob = 0;
+  ret.home_team_stats.dp = [];
+  ret.home_team_stats.doubles = [];
+  ret.home_team_stats.triples = [];
+  ret.home_team_stats.hr = [];
+  ret.home_team_stats.hbp = [];
+  ret.home_team_stats.sb = [];
+  ret.home_team_stats.cs = [];
+  ret.home_team_stats.errors = [];
 
   ret.box = new Object;
   ret.box.home_team = new Object;
@@ -59,6 +75,7 @@ function getGameSummaryToPlay(game, playIndex) {
       let defenseLineup = null;
       let offenseBattingOrder = null;
       let defenseBattingOrder = null;
+      let defensePositionPlayers = null;
 
       if (play.batting_team_id === game.home_team.team_id) {
         offenseTeamStats = ret.home_team_stats;
@@ -67,6 +84,7 @@ function getGameSummaryToPlay(game, playIndex) {
         offenseBattingOrder = currentLineups.current_home_batting_order;
         defenseLineup = ret.box.visitor_team.batting;
         defenseBattingOrder = currentLineups.current_visitor_batting_order;
+        defensePositionPlayers = currentLineups.current_visitor_defense;
       } else if (play.batting_team_id === game.visitor_team.team_id) {
         offenseTeamStats = ret.visitor_team_stats;
         defenseTeamStats = ret.home_team_stats;
@@ -74,6 +92,7 @@ function getGameSummaryToPlay(game, playIndex) {
         offenseBattingOrder = currentLineups.current_visitor_batting_order;
         defenseLineup = ret.box.home_team.batting;
         defenseBattingOrder = currentLineups.current_home_batting_order;
+        defensePositionPlayers = currentLineups.current_home_defense;
       } else {
         console.error("Unrecognized team: " + play.batting_team_id);
       }
@@ -81,7 +100,7 @@ function getGameSummaryToPlay(game, playIndex) {
       offenseTeamStats.runs_per_inning[play.inning-1] = offenseTeamStats.runs_per_inning[play.inning-1] == null ? 0 : offenseTeamStats.runs_per_inning[play.inning-1] + play.enhanced_play.runs;
       offenseTeamStats.score += play.enhanced_play.runs;
       offenseTeamStats.hits += (["S","D","T","H","HR"].includes(play.enhanced_play.playCode));
-      defenseTeamStats.errors += play.enhanced_play.errors.length;
+      defenseTeamStats.error_count += play.enhanced_play.errors.length;
 
       let findPlayerStatsArray = function(playerToFind, battingOrder, lineup) {
         let lineupSpot = null;
@@ -148,6 +167,67 @@ function getGameSummaryToPlay(game, playIndex) {
         let inningLob = play.enhanced_play.basesOccupiedAfterPlay.reduce(function(accumulator, value) { return accumulator += (value != null); }, 0);
         offenseTeamStats.lob += inningLob;
       }
+
+      if (play.enhanced_play.doublePlay) {
+        let fielders = [];
+        let players = play.enhanced_play.outs.forEach(function(out) {
+          out.assistFielders.forEach(function(fielder) {
+            if (fielders.length == 0 || fielders.slice(-1)[0] !== fielder.fielderId) {
+              fielders.push(fielder.fielderId);
+            }
+          });
+          if (fielders.length == 0 || fielders.slice(-1)[0] !== out.putoutFielderId) {
+            fielders.push(out.putoutFielderId);
+          }
+        });
+        offenseTeamStats.dp.push(fielders);
+      }
+
+      if (play.enhanced_play.playCode === "D") {
+        let dpo = new Object;
+        dpo.batting_player_id = play.batting_player_id;
+        dpo.pitcher_player_id = defensePositionPlayers[0];
+        offenseTeamStats.doubles.push(dpo);
+      }
+      if (play.enhanced_play.playCode === "T") {
+        let dpo = new Object;
+        dpo.batting_player_id = play.batting_player_id;
+        dpo.pitcher_player_id = defensePositionPlayers[0];
+        offenseTeamStats.triples.push(dpo);
+      }
+      if (["HR","H"].includes(play.enhanced_play.playCode)) {
+        let dpo = new Object;
+        dpo.batting_player_id = play.batting_player_id;
+        dpo.pitcher_player_id = defensePositionPlayers[0];
+        offenseTeamStats.hr.push(dpo);
+      }
+      if (play.enhanced_play.playCode === "HP") {
+        let dpo = new Object;
+        dpo.batting_player_id = play.batting_player_id;
+        dpo.pitcher_player_id = defensePositionPlayers[0];
+        offenseTeamStats.hbp.push(dpo);
+      }
+      if (play.enhanced_play.playCode === "SB") {
+        let base = play.enhanced_play.rawEvent.basicPlay.replace(/^SB([23H])/, "$1");
+        let runner_player_id = null;
+        if (base === "H") {
+          base = "Home";
+          runner_player_id = play.enhanced_play.runsScoredBy[0].runner;
+        } else {
+          runner_player_id = play.enhanced_play.basesOccupiedAfterPlay[Number.parseInt(base)-1];
+          base = base + "d";
+        }
+        let dpo = new Object;
+        dpo.base = base;
+        dpo.runner_player_id = runner_player_id;
+        dpo.pitcher_player_id = defensePositionPlayers[0];
+        dpo.catcher_player_id = defensePositionPlayers[1];
+        offenseTeamStats.sb.push(dpo);
+      }
+
+      defenseTeamStats.errors = defenseTeamStats.errors.concat(play.enhanced_play.errors);
+
+      // end of play tabulation
 
     } else if (play.type === "substitution") {
       currentLineups = Game.applySubstitution(game, currentLineups, play);
